91d838b7e9f69e244d2a0e79ea05a36dca0a2a21
jonasc@nixos <carewj@oregonstate.edu>
2024-12-19 01:41:37 -0600

    Linked List position function

--- src/list.zig
+++ src/list.zig
@@ -54,10 +54,23 @@ pub fn List(comptime T: type) type {
             self.private.head = node.next;
             return node.value;
         }
+
+        pub fn position(self: Self, value: T, cmp: *const fn (a: T, b: T) bool) ?u32 {
+            var node: ?*Node = self.private.head;
+            var i: u32 = 0;
+            while (node != null) : ({
+                node = node.?.next;
+                i += 1;
+            }) {
+                if (cmp(node.?.value, value)) return i;
+            }
+            return null;
+        }
     };
 }
 
 test "List" {
+    // init the Arena Allocator and GPA
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
 
@@ -67,15 +80,32 @@ test "List" {
         if (deinit_status == .leak) testing.expect(false) catch @panic("TEST FAIL");
     }
 
+    // Set the used allocator
     const allocator = arena.allocator();
 
-    var list = List(u32).create(allocator);
+    // Test Creating a List
+    var list = List(i32).create(allocator);
     try testing.expect(list.private.head == null);
 
+    // Test pushing to a List
     try list.push(1);
+    try list.push(3);
+    try list.push(2);
     try testing.expect(list.private.head != null);
-    try testing.expect(list.private.head.?.value == 1);
+    try testing.expect(list.private.head.?.value == 2);
 
-    const x: u32 = try list.pop();
-    try testing.expect(x == 1);
+    // Test popping from a List
+    const pop: i32 = try list.pop();
+    try testing.expect(pop == 2);
+
+    // Test finding a position in a List
+    const cmp = struct {
+        fn cmp(a: i32, b: i32) bool {
+            return a == b;
+        }
+    }.cmp;
+    const pos3: ?u32 = list.position(3, cmp);
+    try testing.expect(pos3.? == 0);
+    const pos2: ?u32 = list.position(2, cmp);
+    try testing.expect(pos2 == null);
 }
